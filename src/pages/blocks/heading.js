1605cb2bd94e63073d50299e43ee168d4bb39615
Arav <arav.1tb@gmail.com>
2023-06-28 22:53:24 -0400

    heading!

---
+++ src/pages/blocks/heading.js
@@ -0,0 +1,11 @@
+export default function Heading() {
+    return (
+        <div className="mt-14 mr-8">
+        <h1 className="text-4xl font-bold text-center text-rose-500 mr-8">Slack Showcase!</h1>
+        <p className="text-center text-white-500">
+           A collection of <b> personal </b>, and <b> community</b> channels around the Hack Club Slack!
+        </p>
+        </div>
+
+    )
+}
